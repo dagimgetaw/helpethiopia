5100ad97004c397fb840c9d3918c8a91896b902e
Dagim <dagimgetaW27@gmail.com>
2025-12-19 20:41:05 +0300

    docs: add team data along with image

--- client/src/constant/index.js
+++ client/src/constant/index.js
@@ -8,6 +8,12 @@ import {
   Airplay,
   Pill,
 } from "@phosphor-icons/react";
+import feron from "../assets/feron.webp";
+import betelhem from "../assets/betelhem.webp";
+import samuel_m from "../assets/samuel_m.webp";
+import gihon from "../assets/gihon.webp";
+import kidist from "../assets/kidist.webp";
+import userIcon from "../assets/user.webp";
 
 export const nav_link = [
   { path: "/", label: "Home" },
@@ -60,5 +66,81 @@ export const donation_type = [
     title: "Critical Surgical Supplies",
     desc: "Equip hospitals with the tools needed for safe surgeries.",
     icon: Pill,
+  },
+];
+
+export const team_data = [
+  {
+    image: feron,
+    name: "Feron Getachew, MD",
+    position: "President",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: betelhem,
+    name: "Betelhem Berhanu, MD",
+    position: "Vice President",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: samuel_m,
+    name: "Samuel Masresha, MD",
+    position: "Secretary",
+    Biography: "Biography",
+    facebook: "link1",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: gihon,
+    name: "Ghion Ashenafi",
+    position: "Education and Leadership Program Manager",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: kidist,
+    name: "Kidist Solomon",
+    position: "Operation Manager",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: userIcon,
+    name: "Ermias Aklilu",
+    position: "Project Manager",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: userIcon,
+    name: "Dagim Getaw",
+    position: "Full-Stack Developer",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: userIcon,
+    name: "Ahlam Zeynu",
+    position: "Full-Stack Developer",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
+  },
+  {
+    image: userIcon,
+    name: "Elham",
+    position: "Full-Stack Developer",
+    Biography: "Biography",
+    linkedin: "link2",
+    gmail: "link3",
   },
 ];
